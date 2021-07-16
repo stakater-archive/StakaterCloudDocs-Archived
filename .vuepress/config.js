671932f060cadf15dcbdcab6750db70451b25cd7
Waleed Malik <ahmedwaleedmalik@gmail.com>
2021-07-16 14:23:05 +0500

    Add policies documentation

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,186 +1,178 @@
 module.exports = {
-    title: 'Stakater App Agility Platform Documentation',
-    description: 'Stakater App Agility Platform Documentation',
-    head: [
-        ['link', { rel: 'icon', href: '/favicon.png' }]
+  title: "Stakater App Agility Platform Documentation",
+  description: "Stakater App Agility Platform Documentation",
+  head: [["link", { rel: "icon", href: "/favicon.png" }]],
+  plugins: [
+    [
+      "@vuepress/active-header-links",
+      "@vuepress/medium-zoom",
+      "@vuepress/back-to-top",
+      "vuepress-plugin-container",
     ],
-    plugins: [
-        [
-            '@vuepress/active-header-links',
-            '@vuepress/medium-zoom',
-            '@vuepress/back-to-top',
-            'vuepress-plugin-container'
+  ],
+  markdown: {
+    lineNumbers: true,
+    anchor: { permalink: false },
+    // options for markdown-it-toc
+    toc: { includeLevel: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] },
+  },
+  themeConfig: {
+    sidebar: [
+      {
+        title: "Introduction",
+        children: [
+          "/content/sre/introduction/introduction.md",
+          "/content/sre/introduction/why-sro.md",
+          "/content/sre/introduction/sro-overview.md",
+          "/content/sre/introduction/sro-features.md",
+          "/content/sre/introduction/sro-key-differentiators.md",
         ],
+      },
+      {
+        title: "Managed AddOns",
+        children: ["/content/sre/addons/introduction.md"],
+      },
+      {
+        title: "Cloud Provider",
+        children: [
+          "/content/sre/cloud-provider/azure.md",
+          "/content/sre/cloud-provider/aws.md",
+          "/content/sre/cloud-provider/google.md",
+          "/content/sre/cloud-provider/binero.md",
+        ],
+      },
+      {
+        title: "Applications",
+        children: [
+          "/content/sre/applications/cloud-native-app.md",
+          "/content/sre/forecastle/forecastle.md",
+        ],
+      },
+      {
+        title: "GitOps",
+        children: [
+          "/content/sre/gitops/introduction.md",
+          "/content/sre/gitops/github.md",
+          "/content/sre/gitops/gitlab.md",
+          "/content/sre/gitops/faqs.md",
+        ],
+      },
+      {
+        title: "Onboarding",
+        children: [
+          "/content/sre/onboarding/team-onboarding.md",
+          "/content/sre/onboarding/application-onboarding.md",
+          "/content/sre/onboarding/developer-training.md",
+        ],
+      },
+      {
+        title: "Authentication and Authorization",
+        children: [
+          "/content/sre/authentication-authorization/google-idp.md",
+          "/content/sre/authentication-authorization/azure-idp.md",
+          "/content/sre/authentication-authorization/keycloak-idp.md",
+          "/content/sre/authentication-authorization/saml-idp.md",
+        ],
+      },
+      {
+        title: "Continuous Integration (CI)",
+        children: [
+          "/content/sre/pipelines/introduction.md",
+          "/content/sre/pipelines/openshift-pipelines/openshift-pipelines.md",
+          "/content/sre/pipelines/openshift-pipelines/deploying-delivery-pipeline.md",
+          "/content/sre/pipelines/openshift-pipelines/deploying-cicd-pipeline.md",
+        ],
+      },
+      {
+        title: "Artifacts Management",
+        children: [
+          "/content/sre/registry/registry.md",
+          "/content/sre/registry/accessing-registry.md",
+          "/content/sre/repository/nexus.md",
+        ],
+      },
+      {
+        title: "Code Quality",
+        children: ["/content/sre/code-quality/sonarqube.md"],
+      },
+      {
+        title: "Logging",
+        children: [
+          "/content/sre/logging/logging.md",
+          "/content/sre/logging/kibana-view-logs.md",
+        ],
+      },
+      {
+        title: "Monitoring",
+        children: [
+          "/content/sre/monitoring/app-uptime.md",
+          "/content/sre/monitoring/app-alerts.md",
+          "/content/sre/monitoring/goldilocks.md",
+          "/content/sre/monitoring/kube-resource-report.md",
+        ],
+      },
+      {
+        title: "Alerting",
+        children: [
+          "/content/sre/alerting/downtime-notifications-uptimerobot.md",
+          "/content/sre/alerting/log-alerts.md",
+          "/content/sre/alerting/workload-application-alerts.md",
+        ],
+      },
+      {
+        title: "Autoscaling",
+        children: ["/content/sre/autoscaling/autoscaling.md"],
+      },
+      {
+        title: "Backup & Restore",
+        children: [
+          "/content/sre/backup-restore/velero-cli.md",
+          "/content/sre/backup-restore/backup-restore.md",
+          "/content/sre/backup-restore/stateful-app-example.md",
+          "/content/sre/backup-restore/restore-with-gitops.md",
+        ],
+      },
+      {
+        title: "Networking",
+        children: [
+          "/content/sre/networking/routes.md",
+          "/content/sre/networking/route-sharding.md",
+          "/content/sre/networking/external-dns.md",
+        ],
+      },
+      {
+        title: "Secrets Management",
+        children: [
+          "/content/sre/secrets/introduction.md",
+          "/content/sre/secrets/sealed-secrets.md",
+          "/content/sre/secrets/vault.md",
+        ],
+      },
+      {
+        title: "Security",
+        children: ["/content/sre/security/policies/policies.md"],
+      },
+      {
+        title: "Cluster Configuration",
+        children: ["/content/sre/cluster-configuration/node-configuration.md"],
+      },
+      {
+        title: "Support",
+        children: ["/content/sre/support/support.md"],
+      },
+      {
+        title: "Frequently Asked Questions",
+        children: [
+          "/content/sre/faq/product.md",
+          "/content/sre/faq/operations.md",
+          "/content/sre/faq/purchasing.md",
+          "/content/sre/faq/customization.md",
+          "/content/sre/faq/developers.md",
+        ],
+      },
     ],
-    markdown: {
-        lineNumbers: true,
-        anchor: { permalink: false },
-        // options for markdown-it-toc
-        toc: { includeLevel: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] }
-    },
-    themeConfig: {
-        sidebar: [
-            {
-                title: 'Introduction',
-                children: [
-                    '/content/sre/introduction/introduction.md',
-                    '/content/sre/introduction/why-sro.md',
-                    '/content/sre/introduction/sro-overview.md',
-                    '/content/sre/introduction/sro-features.md',                  
-                    '/content/sre/introduction/sro-key-differentiators.md'
-                ]
-            },
-            {
-                title: 'Managed AddOns',
-                children: [
-                    '/content/sre/addons/introduction.md'
-                ]
-            },            
-            {
-                title: 'Cloud Provider',
-                children: [
-                    '/content/sre/cloud-provider/azure.md',
-                    '/content/sre/cloud-provider/aws.md', 
-                    '/content/sre/cloud-provider/google.md',
-                    '/content/sre/cloud-provider/binero.md'                    
-                ]
-            },
-            {
-                title: 'Applications',
-                children: [
-                    '/content/sre/applications/cloud-native-app.md',
-                    '/content/sre/forecastle/forecastle.md'
-                ]
-            },
-            {
-                title: 'GitOps',
-                children: [
-                    '/content/sre/gitops/introduction.md',                    
-                    '/content/sre/gitops/github.md',
-                    '/content/sre/gitops/gitlab.md',       
-                    '/content/sre/gitops/faqs.md'                    
-                ]
-            },
-            {
-                title: 'Onboarding',
-                children: [
-                  '/content/sre/onboarding/team-onboarding.md',
-                  '/content/sre/onboarding/application-onboarding.md',
-                  '/content/sre/onboarding/developer-training.md'
-                ]
-            },
-            {
-                title: 'Authentication and Authorization',
-                children: [
-                    '/content/sre/authentication-authorization/google-idp.md',
-                    '/content/sre/authentication-authorization/azure-idp.md',
-                    '/content/sre/authentication-authorization/keycloak-idp.md',
-                    '/content/sre/authentication-authorization/saml-idp.md'
-                ]
-            },
-            {
-                title: 'Continuous Integration (CI)',
-                children: [
-                    '/content/sre/pipelines/introduction.md',
-                    '/content/sre/pipelines/openshift-pipelines/openshift-pipelines.md',
-                    '/content/sre/pipelines/openshift-pipelines/deploying-delivery-pipeline.md',
-                    '/content/sre/pipelines/openshift-pipelines/deploying-cicd-pipeline.md'
-                ]
-            },
-            {
-                title: 'Artifacts Management',
-                children: [
-                    '/content/sre/registry/registry.md',
-                    '/content/sre/registry/accessing-registry.md',
-                    '/content/sre/repository/nexus.md'                    
-                ]
-            },
-            {
-                title: 'Code Quality',
-                children: [
-                    '/content/sre/code-quality/sonarqube.md'
-                ]
-            },
-            {
-                title: 'Logging',
-                children: [
-                    '/content/sre/logging/logging.md',
-                    '/content/sre/logging/kibana-view-logs.md'
-                ]
-            },                        
-            {
-                title: 'Monitoring',
-                children: [
-                    '/content/sre/monitoring/app-uptime.md',
-                    '/content/sre/monitoring/app-alerts.md',
-                    '/content/sre/monitoring/goldilocks.md',
-                    '/content/sre/monitoring/kube-resource-report.md'
-                ]
-            },
-            {
-                title: 'Alerting',
-                children: [
-                    '/content/sre/alerting/downtime-notifications-uptimerobot.md',
-                    '/content/sre/alerting/log-alerts.md',
-                    '/content/sre/alerting/workload-application-alerts.md'
-                ]                
-            },            
-            {
-                title: 'Autoscaling',
-                children: [
-                    '/content/sre/autoscaling/autoscaling.md'
-                ]
-            },
-            {
-                title: 'Backup & Restore',
-                children: [
-                    '/content/sre/backup-restore/velero-cli.md',
-                    '/content/sre/backup-restore/backup-restore.md',
-                    '/content/sre/backup-restore/stateful-app-example.md',
-                    '/content/sre/backup-restore/restore-with-gitops.md'
-                ]
-            },
-            {
-                title: 'Networking',
-                children: [
-                    '/content/sre/networking/routes.md',
-                    '/content/sre/networking/route-sharding.md',
-                    '/content/sre/networking/external-dns.md'
-                ]
-            },
-            {
-                title: 'Secrets Management',
-                children: [
-                    '/content/sre/secrets/introduction.md',
-                    '/content/sre/secrets/sealed-secrets.md',
-                    '/content/sre/secrets/vault.md'
-                ]
-            },
-            {
-                title: 'Cluster Configuration',
-                children: [
-                    '/content/sre/cluster-configuration/node-configuration.md',
-                ]
-            },
-            {
-                title: 'Support',
-                children: [
-                    '/content/sre/support/support.md'
-                ]
-            },
-            {
-                title: 'Frequently Asked Questions',
-                children: [
-                    '/content/sre/faq/product.md',
-                    '/content/sre/faq/operations.md',
-                    '/content/sre/faq/purchasing.md',
-                    '/content/sre/faq/customization.md',
-                    '/content/sre/faq/developers.md'
-                ]
-            }
-        ],
-        repo: 'stakater/stakaterclouddocs',
-        editLinks: true,
-        editLinkText: 'Help us improve this page!'
-    }
-}
+    repo: "stakater/stakaterclouddocs",
+    editLinks: true,
+    editLinkText: "Help us improve this page!",
+  },
+};
--- content/sre/security/policies/policies.md
+++ content/sre/security/policies/policies.md
@@ -1,3 +1,82 @@
 # Policies
 
-_TBD_
+## Overview
+
+Policies are resources that govern the behavior of the Kubernetes cluster, where they provide defaults for configuration and also determines what is allowed or disallowed.
+
+![image](./images/how-policies-work.png)
+
+## Kyverno
+
+Kyverno is a policy engine designed for Kubernetes. It manages policies as Kubernetes native resources and no new language is required to write policies. This allows using familiar tools such as kubectl, git, and kustomize to manage policies. Kyverno policies can validate, mutate, and generate Kubernetes resources.
+
+## How to write Policies?
+
+[Create custom policy rules to validate, mutate, and generate configurations.](https://kyverno.io/docs/writing-policies)
+
+[For reference: Sample Policies](https://kyverno.io/policies/)
+
+## Architecture Diagram
+
+![image](./images/managed-policies-architecture-diagram.png)
+
+### Example
+
+```yaml
+apiVersion: kyverno.io/v1
+kind: Policy
+metadata:
+  name: require-label-development-team
+  annotations:
+    policies.kyverno.io/title: Require label Development Team
+    policies.kyverno.io/category: Best Practices
+    policies.kyverno.io/severity: low
+    policies.kyverno.io/subject: Pod
+    policies.kyverno.io/description: >-
+      Require pods to have development-team label set
+spec:
+  validationFailureAction: enforce
+  rules:
+    - name: require-label-development-team
+      match:
+        resources:
+          kinds:
+            - Pod
+      validate:
+        message: "The label `development-team` is required"
+        anyPattern:
+          - metadata:
+              labels:
+                development-team: "?*"
+```
+
+### Default Policy
+
+Policies that are enforced by default. User cannot disable these policies since they are considered essential for governance, security etc.
+
+## Front-End
+
+A front-end view of how users can manage their policies.
+![image](./images/policies-frontend.png)
+
+## Kyverno vs OPA Gatekeeper
+
+| Features/Capabilities                       | Gatekeeper | Kyverno        |
+| ------------------------------------------- | ---------- | -------------- |
+| Validation                                  | ✓          | ✓              |
+| Mutation                                    | ✓          | ✓              |
+| Generation                                  | X          | ✓              |
+| Policy as native resources                  | ✓          | ✓              |
+| Metrics exposed                             | ✓          | ✓              |
+| OpenAPI validation schema (kubectl explain) | X          | ✓              |
+| High Availability                           | ✓          | ✓              |
+| API object lookup                           | ✓          | ✓              |
+| CLI with test ability                       | ✓          | ✓              |
+| Policy audit ability                        | ✓          | ✓              |
+| Programming required                        | ✓ (Rego)   | ✓ (Javascript) |
+
+## Useful Links
+
+- [https://kubernetes.io/docs/concepts/policy](https://kubernetes.io/docs/concepts/policy/)
+- [OPA Gatekeeper vs. Kyverno](https://www.youtube.com/watch?v=9gSrRNmmKBc)
+- [Kyverno Policy Agent](https://thenewstack.io/kyverno-kubernetes-configuration-via-policy/)
